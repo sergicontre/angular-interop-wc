a3ca3ba5c813eead6e81d2ac3f971acf7b903db8
sergicontre <sergicontre@users.noreply.github.com>
2019-04-14 22:19:10 +0200

    include wc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'my-credit-card'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-interop-wc';
+  title = 'My LitElement Bank';
+  info = '';
+  sendInfo(event) {
+    console.log(`Hello, my name is!`,event);
+    this.info = JSON.stringify(event.detail);
+  }
 }
